c97f0d3eecf958304aa9cc1013ee747729f41d7a
Ben Clark <hello@clarkben.com>
2022-12-03 16:05:29 +0000

    Tidy up of day2 code

--- day2/puzzle2/main.js
+++ day2/puzzle2/main.js
@@ -3,23 +3,13 @@ import {readFileSync} from 'fs';
 const scores = [['X', 1], ['Y', 2], ['Z', 3]];
 const combinationOutcomes = [['A X', 0], ['A Y', 3], ['A Z', 6], ['B X', 0], ['B Y', 3], ['B Z', 6], ['C X', 0], ['C Y', 3], ['C Z', 6]];
 const combinationHands = [['A X', 'Z'], ['A Y', 'X'], ['A Z', 'Y'], ['B X', 'X'], ['B Y', 'Y'], ['B Z', 'Z'], ['C X', 'Y'], ['C Y', 'Z'], ['C Z', 'X']];
-const lookupScores = new Map;
-const lookupOutcomes = new Map;
-const lookupHands = new Map;
-
-scores.forEach(array => {
-    lookupScores.set(...array);
-});
-combinationOutcomes.forEach(array => {
-    lookupOutcomes.set(...array);
-});
-combinationHands.forEach(array => {
-    lookupHands.set(...array);
-});
+const lookupScores = new Map(scores);
+const lookupOutcomes = new Map(combinationOutcomes);
+const lookupHands = new Map(combinationHands);
 
 const gameArray = readFileSync('input.txt').toString().split("\n");
 let totalScore = 0;
 gameArray.forEach(combination => {
     totalScore += lookupOutcomes.get(combination) + lookupScores.get(lookupHands.get(combination));
 })
-console.log(totalScore);
+console.log(totalScore)

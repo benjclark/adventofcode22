2d6a8c1f78e073e316ee4d657e6d0c1c321e18eb
Ben Clark <hello@clarkben.com>
2022-12-04 11:23:18 +0000

    Tidies up code for day2

--- day2/puzzle1/main.js
+++ day2/puzzle1/main.js
@@ -1,13 +1,11 @@
 import {readFileSync} from 'fs';
 
-const scores = [['X', 1], ['Y', 2], ['Z', 3]];
-const combinationOutcomes = [['A X', 3], ['A Y', 6], ['A Z', 0], ['B X', 0], ['B Y', 3], ['B Z', 6], ['C X', 6], ['C Y', 0], ['C Z', 3]];
-const lookupScores = new Map(scores);
-const lookupOutcomes = new Map(combinationOutcomes);
-
-const gameArray = readFileSync('input.txt').toString().split("\n");
+const gameData = readFileSync('input.txt').toString().split("\n");
+const lookupScores = new Map([['X', 1], ['Y', 2], ['Z', 3]]);
+const lookupOutcomes = new Map([['A X', 3], ['A Y', 6], ['A Z', 0], ['B X', 0], ['B Y', 3], ['B Z', 6], ['C X', 6], ['C Y', 0], ['C Z', 3]]);
 let totalScore = 0;
-gameArray.forEach(combination => {
+
+gameData.forEach(combination => {
     totalScore += lookupOutcomes.get(combination) + lookupScores.get(combination[2]);
 })
 console.log(totalScore);
--- day2/puzzle2/main.js
+++ day2/puzzle2/main.js
@@ -1,15 +1,12 @@
 import {readFileSync} from 'fs';
 
-const scores = [['X', 1], ['Y', 2], ['Z', 3]];
-const combinationOutcomes = [['A X', 0], ['A Y', 3], ['A Z', 6], ['B X', 0], ['B Y', 3], ['B Z', 6], ['C X', 0], ['C Y', 3], ['C Z', 6]];
-const combinationHands = [['A X', 'Z'], ['A Y', 'X'], ['A Z', 'Y'], ['B X', 'X'], ['B Y', 'Y'], ['B Z', 'Z'], ['C X', 'Y'], ['C Y', 'Z'], ['C Z', 'X']];
-const lookupScores = new Map(scores);
-const lookupOutcomes = new Map(combinationOutcomes);
-const lookupHands = new Map(combinationHands);
-
-const gameArray = readFileSync('input.txt').toString().split("\n");
+const gameData = readFileSync('input.txt').toString().split("\n");
+const lookupScores = new Map([['X', 1], ['Y', 2], ['Z', 3]]);
+const lookupOutcomes = new Map([['A X', 0], ['A Y', 3], ['A Z', 6], ['B X', 0], ['B Y', 3], ['B Z', 6], ['C X', 0], ['C Y', 3], ['C Z', 6]]);
+const lookupHands = new Map([['A X', 'Z'], ['A Y', 'X'], ['A Z', 'Y'], ['B X', 'X'], ['B Y', 'Y'], ['B Z', 'Z'], ['C X', 'Y'], ['C Y', 'Z'], ['C Z', 'X']]);
 let totalScore = 0;
-gameArray.forEach(combination => {
+
+gameData.forEach(combination => {
     totalScore += lookupOutcomes.get(combination) + lookupScores.get(lookupHands.get(combination));
 })
 console.log(totalScore)

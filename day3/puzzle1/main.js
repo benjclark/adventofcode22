24559138a93cf67eec2a3f35c462d3dea6dae675
Ben Clark <hello@clarkben.com>
2022-12-04 11:11:49 +0000

    Tidies up code for day3

--- day3/puzzle1/main.js
+++ day3/puzzle1/main.js
@@ -1,19 +1,21 @@
 import {readFileSync} from 'fs';
 
 const bagsContentsData = readFileSync('input.txt').toString().split("\n");
-
 const priorityLookup = new Map();
+let totalScore;
 
-for (let i = 0; i < 26; i++) {
-    // a-z
-    priorityLookup.set(String.fromCharCode(97 + i), i + 1)
-}
-for (let j = 0; j < 26; j++) {
-    // A-Z
-    priorityLookup.set(String.fromCharCode(65 + j), j + 27)
+function setupAlphabetScoringLookup() {
+    for (let i = 0; i < 26; i++) {
+        // a-z
+        priorityLookup.set(String.fromCharCode(97 + i), i + 1)
+    }
+    for (let j = 0; j < 26; j++) {
+        // A-Z
+        priorityLookup.set(String.fromCharCode(65 + j), j + 27)
+    }
 }
 
-function findDuplicateChar(stringOne, stringTwo) {
+function findDuplicateCharacter(stringOne, stringTwo) {
     return [...stringOne].filter(character => {
         if (stringTwo.indexOf(character) > -1) {
             return character;
@@ -21,12 +23,23 @@ function findDuplicateChar(stringOne, stringTwo) {
     })[0];
 }
 
-let total = 0;
+function findCommonCharacters() {
+    return bagsContentsData.map(data => {
+        const firstHalfString = data.substring(0, data.length / 2);
+        const secondHalfString = data.substring(data.length / 2, data.length);
+        return findDuplicateCharacter(firstHalfString, secondHalfString);
+    });
+}
 
-bagsContentsData.forEach(data => {
-    const firstHalfString = data.substring(0, data.length / 2);
-    const secondHalfString = data.substring(data.length / 2, data.length);
-    total += priorityLookup.get(findDuplicateChar(firstHalfString, secondHalfString));
-});
+function calculateTotalScoreForCharacters(characters) {
+    let total = 0;
+    characters.forEach(character => {
+        total += priorityLookup.get(character);
+    });
+    return total;
+}
 
-console.log(total);
+setupAlphabetScoringLookup();
+totalScore = calculateTotalScoreForCharacters(findCommonCharacters());
+
+console.log(totalScore);
--- day3/puzzle2/main.js
+++ day3/puzzle2/main.js
@@ -1,28 +1,31 @@
 import {readFileSync} from 'fs';
 
 const bagsContentsData = readFileSync('input.txt').toString().split("\n");
-
 const priorityLookup = new Map();
-
-for (let i = 0; i < 26; i++) {
-    // a-z
-    priorityLookup.set(String.fromCharCode(97 + i), i + 1)
-}
-for (let j = 0; j < 26; j++) {
-    // A-Z
-    priorityLookup.set(String.fromCharCode(65 + j), j + 27)
-}
-
-// put everything in groups of 3
 const groups = [[]];
-bagsContentsData.forEach(data => {
-    if (groups[groups.length - 1].length === 3) {
-        groups.push([]);
-    }
-    groups[groups.length - 1].push(data);
-});
+let totalScore;
 
-function findDuplicateChar(stringOne, stringTwo) {
+function setupAlphabetScoringLookup() {
+    for (let i = 0; i < 26; i++) {
+        // a-z
+        priorityLookup.set(String.fromCharCode(97 + i), i + 1)
+    }
+    for (let j = 0; j < 26; j++) {
+        // A-Z
+        priorityLookup.set(String.fromCharCode(65 + j), j + 27)
+    }
+}
+
+function createGroupsOfThreeFromData() {
+    bagsContentsData.forEach(data => {
+        if (groups[groups.length - 1].length === 3) {
+            groups.push([]);
+        }
+        groups[groups.length - 1].push(data);
+    });
+}
+
+function findDuplicateCharacters(stringOne, stringTwo) {
     return [...stringOne].filter(character => {
         if (stringTwo.indexOf(character) > -1) {
             return character;
@@ -30,18 +33,25 @@ function findDuplicateChar(stringOne, stringTwo) {
     });
 }
 
-// find common character per group of 3
-const commonCharactersPerGroup = groups.map(group => {
-    const common = findDuplicateChar(group[0], group[1])
-    return common.filter(character => {
-        return group[2].indexOf(character) > -1;
+function findCommonCharacterPerGroup() {
+    return groups.map(group => {
+        const common = findDuplicateCharacters(group[0], group[1])
+        return common.filter(character => {
+            return group[2].indexOf(character) > -1;
+        })[0];
     });
-});
+}
 
-// calculate score for all common characters
-let total = 0;
-commonCharactersPerGroup.forEach(characterArray => {
-    total += priorityLookup.get(characterArray[0]);
-});
+function calculateTotalScoreForCharacters(commonCharacters) {
+    let total = 0;
+    commonCharacters.forEach(character => {
+        total += priorityLookup.get(character);
+    });
+    return total;
+}
 
-console.log(total);
+setupAlphabetScoringLookup();
+createGroupsOfThreeFromData();
+totalScore = calculateTotalScoreForCharacters(findCommonCharacterPerGroup());
+
+console.log(totalScore);
